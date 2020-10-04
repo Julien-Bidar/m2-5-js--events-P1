573bd1a952b59a83f3a7a5aa83b7a7660739f65e
Julien Bidar <julien.bidar@gmail.com>
2020-10-04 14:48:51 -0400

    event P1 over

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -3,6 +3,11 @@
 console.log('exercise 1.1');
 
 let body = document.querySelector("body");
+body.style.cssText = "background: radial-gradient(circle, rgba(65,96,177,1) 0%, rgba(107,176,190,1) 100%); text-align: center"
+let title = body.appendChild(document.createElement("h1"))
+title.textContent = "Be a faster clicker!!!"
+title.style.cssText = "color: white; margin: 25px; font-size: 45px"
+
 
 let win = false
 let lose = false
@@ -11,6 +16,7 @@ const display = () =>{
     let disp = document.createElement("h1");
     if (!lose){        
         disp.innerText = "You Win!"
+        disp.style.cssText = "color: pink; margin: 75px; font-size: 45px"
         win = true
     }
     body.appendChild(disp)
@@ -24,6 +30,7 @@ setTimeout (() => {
     if(!win){
         let youLose = document.createElement('h1')
         youLose.innerText = "You Lose!"
+        youLose.style.cssText = "color: red; margin: 75px; font-size: 45px"
         body.appendChild(youLose)
     }
 }, 1000)
--- workshop/exercise-1.2/app.js
+++ workshop/exercise-1.2/app.js
@@ -2,6 +2,7 @@
 // ------------
 console.log('exercise 1.2');
 let body = document.querySelector("body")
+body.style.cssText = "background: radial-gradient(circle, rgba(65,96,177,1) 0%, rgba(107,176,190,1) 100%); text-align: center"
 let win = false
 let lose = false
 
@@ -10,11 +11,15 @@ let secDisp = Math.round(sec/1000);
 
 const span = document.getElementById("time");
 span.innerText = secDisp
+span.style.cssText = "color: yellow; margin: 25px; font-size: 45px; font-weight: bold"
+const timeText = document.querySelector(".time-text")
+timeText.style.cssText = "color: white; margin: 25px; font-size: 45px; font-weight: bold"
 
 const dispWin = ()=>{
     if(!lose){
         const winH1 = document.createElement("h1")
         winH1.innerText = "You win!"
+        winH1.style.cssText = "color: pink; margin: 80px; font-size: 80px; font-weight: bold"
         body.appendChild(winH1)
         win = true
     }
@@ -27,6 +32,7 @@ setTimeout(() =>{
     if(!win){
         const loseH1 = document.createElement("h1")
         loseH1.innerText = "You Lose!"
+        loseH1.style.cssText = "color: red; margin: 75px; font-size: 45px"
         body.appendChild(loseH1)
     }
 }, sec)
